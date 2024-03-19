5c793acd59da7f1d766da2d4bda0fdb536f09ec2
Tan Jie Ling <tanjieling3@gmail.com>
2024-03-20 03:54:59 +0800

    Add more JUnit tests

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,15 +1,19 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
+import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalNetConnect;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -22,6 +26,27 @@ import seedu.address.model.person.Remark;
 public class RemarkCommandTest {
 
     private Model model = new ModelManager(getTypicalNetConnect(), new UserPrefs());
+
+    @Test
+    public void execute_invalidIndex_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        Remark remark = new Remark("Some remark");
+
+        RemarkCommand remarkCommand = new RemarkCommand(outOfBoundIndex, remark);
+        assertThrows(CommandException.class, () -> remarkCommand.execute(model));
+    }
+
+    @Test
+    public void execute_validIndexAndRemark_success() throws CommandException {
+        Index validIndex = Index.fromOneBased(1); // Assuming at least one person in the list
+        Remark remark = new Remark("New remark");
+
+        RemarkCommand remarkCommand = new RemarkCommand(validIndex, remark);
+        CommandResult commandResult = remarkCommand.execute(model);
+
+        assertEquals(String.format(RemarkCommand.MESSAGE_ADD_REMARK_SUCCESS, "Alice Pauline"),
+                commandResult.getFeedbackToUser());
+    }
 
     @Test
     public void equals() {
